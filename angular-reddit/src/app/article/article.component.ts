5ce5ff853138bca0cec5d3f2a0ac20a0e1da50cd
Magdalena Fornalczyk <Magdalena Fornalczyk>
2017-06-26 17:14:28 +0200

    added article.model.ts page 70

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding(`attr.class`) cssClass= `row`;
-  constructor() { }
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title=`Angular 2`;
+    this.link = `http://angular.io`;
+    this.votes = 10;
+  }
+
+  voteUp():boolean{
+    this.votes +=1;
+    return false;
+  }
+  voteDown():boolean{
+    this.votes-=1;
+    return false;
+  }
 
   ngOnInit() {
   }
